76697cecc08c93d4ed738e201c1f0244c7b52d63
LWC421 <lwc421@gmail.com>
2022-09-15 17:22:04 +0900

    가장 먼 노드 BFS

---
+++ 가장_먼_노드.js
@@ -0,0 +1,38 @@
+const solution = (n, edge) => {
+  const graph = Array.from(Array(n + 1), () => []);
+
+  for (const [src, dest] of edge) {
+    graph[src].push(dest);
+    graph[dest].push(src);
+  }
+
+  const distance = Array(n + 1).fill(0);
+  distance[1] = 1;
+
+  const queue = [1];
+  while (queue.length > 0) {
+    const src = queue.shift();
+    for (const dest of graph[src]) {
+      if (distance[dest] === 0) {
+        queue.push(dest);
+        distance[dest] = distance[src] + 1;
+      }
+    }
+  }
+  const max = Math.max(...distance);
+
+  return distance.filter((item) => item === max).length;
+};
+
+const n = 6;
+const vertex = [
+  [3, 6],
+  [4, 3],
+  [3, 2],
+  [1, 3],
+  [1, 2],
+  [2, 4],
+  [5, 2],
+];
+
+console.log(solution(n, vertex));
